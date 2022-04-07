27fb8a5cf7f2cf9886e876f16524493ce2426240
Natalie Finnegan <nfinneg2@gmail.com>
2022-04-07 16:34:52 -0500

    starting on intro landing pg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id="app_title">dev_search</h1>
       </header>
+      <aside className="intro">
+        <h3>Looking for programmers?</h3>
+        <p>
+          Search potential candidates based on language tags on their Github
+          repos!
+        </p>
+      </aside>
     </div>
   );
 }
