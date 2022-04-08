34472b8db3894ed548d6cb299f99c372f81b7f0d
Natalie Finnegan <nfinneg2@gmail.com>
2022-04-07 21:56:09 -0500

    working on landing page, added img

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,20 @@ const Header = () => {
         dev_search
       </nav>
       <header className="intro container-fluid">
-        <h1 className="landing_page_text">Looking for programmers?</h1>
-        <p className="landing_page_text">
-          Search potential candidates based on language tags on their Github
-          repos!
-        </p>
+        <div>
+          <h1 className="landing_page_text">Looking for programmers?</h1>
+          <p className="landing_page_text">
+            Search potential candidates based on language tags on their Github
+            repos!
+          </p>
+        </div>
+        <div>
+          <img
+            id="intro_image"
+            src="/assets/intro_image.png"
+            alt="cartoonImage_guy_at_computer"
+          ></img>
+        </div>
       </header>
     </div>
   );
