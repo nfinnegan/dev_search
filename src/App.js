9045a2a2287274398e4af15e44bd36eae6b3a12c
nfinnegan <nfinneg2@gmail.com>
2022-04-26 15:00:40 -0500

    added search component & react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Search from "./components/Search/Search";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -7,6 +9,9 @@ function App() {
     <div className="App">
       <Header />
       <Footer />
+      <Router>
+        <Route path="/search" component={Search} />
+      </Router>
     </div>
   );
 }
